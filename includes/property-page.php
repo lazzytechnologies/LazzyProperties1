1ffaf425c3468594bee7a05d997a2886cf04f947
lazzytechnologies <33306216+lazzytechnologies@users.noreply.github.com>
2017-11-17 14:27:27 +0800

    page

--- includes/property-page.php
+++ includes/property-page.php
@@ -14,8 +14,8 @@
         <!-- property area -->
         <div class="content-area single-property" style="background-color: #FCFCFC;">&nbsp;
             <div class="container">   
-
-                <div class="clearfix padding-top-40" >
+				<?php echo property_page();?>
+                <!--<div class="clearfix padding-top-40" >
 
                     <div class="col-md-8 single-property-content prp-style-1 ">
                      <div class="row">
@@ -119,80 +119,14 @@
                             </div>
                             <!-- .property-meta -->
 
-                            <div class="section">
+                            <!--<div class="section">
                                 <h4 class="s-property-title">Description</h4>
                                 <div class="s-property-content">
                                     <p>Nulla quis dapibus nisl. Suspendisse ultricies Nulla quis dapibus nisl. Suspendisse ultricies commodo arcu nec pretium. Nullam sed arcu ultricies commodo arcu nec pretium. Nullam sed arcu ultricies Nulla quis dapibus nisl. Suspendisse ultricies commodo arcu nec pretium. Nullam sed arcu ultricies Nulla quis dapibus nisl. Suspendisse ultricies commodo arcu nec pretium. Nullam sed arcu ultricies                                </p>
                                 </div>
-                            </div>
-                            <!-- End description area  -->
-
-                            <div class="section additional-details">
-
-                                <h4 class="s-property-title">Additional Details</h4>
-
-                                <ul class="additional-details-list clearfix">
-                                    <li>
-                                        <span class="col-xs-6 col-sm-4 col-md-4 add-d-title">Waterfront</span>
-                                        <span class="col-xs-6 col-sm-8 col-md-8 add-d-entry">Yes</span>
-                                    </li>
-
-                                    <li>
-                                        <span class="col-xs-6 col-sm-4 col-md-4 add-d-title">Built In</span>
-                                        <span class="col-xs-6 col-sm-8 col-md-8 add-d-entry">2003</span>
-                                    </li>
-                                    <li>
-                                        <span class="col-xs-6 col-sm-4 col-md-4 add-d-title">Parking</span>
-                                        <span class="col-xs-6 col-sm-8 col-md-8 add-d-entry">2 Or More Spaces,Covered Parking,Valet Parking</span>
-                                    </li>
-
-                                    <li>
-                                        <span class="col-xs-6 col-sm-4 col-md-4 add-d-title">Waterfront</span>
-                                        <span class="col-xs-6 col-sm-8 col-md-8 add-d-entry">Yes</span>
-                                    </li>
-
-                                    <li>
-                                        <span class="col-xs-6 col-sm-4 col-md-4 add-d-title">View</span>
-                                        <span class="col-xs-6 col-sm-8 col-md-8 add-d-entry">Intracoastal View,Direct ew</span>
-                                    </li>
-
-                                    <li>
-                                        <span class="col-xs-6 col-sm-4 col-md-4 add-d-title">Waterfront Description:</span>
-                                        <span class="col-xs-6 col-sm-8 col-md-8 add-d-entry">Intracoastal Front,Ocean Access</span>
-                                    </li> 
-
-                                </ul>
-                            </div>  
-                            <!-- End additional-details area  -->
-
-                            <div class="section property-features">      
-
-                                <h4 class="s-property-title">Features</h4>                            
-                                <ul>
-                                    <li><a href="properties.html">Swimming Pool</a></li>   
-                                    <li><a href="properties.html">3 Stories</a></li>
-                                    <li><a href="properties.html">Central Cooling</a></li>
-                                    <li><a href="properties.html">Jog Path 2</a></li>
-                                    <li><a href="properties.html">2 Lawn</a></li>
-                                    <li><a href="properties.html">Bike Path</a></li>
-                                </ul>
-
-                            </div>
-                            <!-- End features area  -->
-
-                            <div class="section property-video"> 
-                                <h4 class="s-property-title">Property Video</h4> 
-                                <div class="video-thumb">
-                                    <a class="video-popup" href="yout" title="Virtual Tour">
-                                        <img src="assets/img/property-video.jpg" class="img-responsive wp-post-image" alt="Exterior">            
-                                    </a>
-                                </div>
-                            </div>
-                            <!-- End video area  -->
-                            
-                            
-
-                            <div class="section property-share"> 
+                            </div>-->
+							<!-- Start share area -->
+                            <!--<div class="section property-share"> 
                                 <h4 class="s-property-title">Share width your friends </h4> 
                                 <div class="roperty-social">
                                     <ul> 
@@ -205,9 +139,9 @@
                                         <li><a title="Share this on linkedin " href="#"><img src="assets/img/social_big/linkedin_grey.png"></a></li>                                        
                                     </ul>
                                 </div>
-                            </div>
-                            <!-- End video area  -->
-                            
+                            </div>-->
+                            <!-- End share area  -->
+                           <!-- 
                         </div>
                     </div>
 
@@ -257,11 +191,11 @@
                                                 <li><i class="pe-7s-call strong"> </i> +1 908 967 5906</li>
                                             </ul>
                                             <!--<p>Duis mollis  blandit tempus porttitor curabiturDuis mollis  blandit tempus porttitor curabitur , est non…</p>-->
-                                        </div>
+                                        <!--</div>
 
                                     </div>
                                 </div>
-                            </div>
+                            </div>-->
 
                             <div class="panel panel-default sidebar-menu similar-property-wdg wow fadeInRight animated">
                                 <div class="panel-heading">
